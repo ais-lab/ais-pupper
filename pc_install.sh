3e361924a88e02ce4af00108de4f69fccf7dfd31
nguyenntt97 <rohirrim3105@gmail.com>
2025-12-02 12:50:10 +0900

    ignore vscode and update install instruction

--- pc_install.sh
+++ pc_install.sh
@@ -33,6 +33,18 @@ rosdep install --from-paths src --ignore-src -r -y
 sudo apt install -y ros-humble-teleop-twist-keyboard ros-humble-teleop-twist-joy
 sudo apt install -y ros-humble-v4l2-camera ros-humble-image-transport-plugins
 sudo apt install -y ros-humble-rqt*
+
+######
 sudo apt install ros-humble-vision-msgs # fix vision_msgsConfig.cmake err
+###### RUN THIS IF YOU GET vision_msgsConfig.cmake NOT FOUND ERROR
+# 1. Remove the old key file first
+sudo rm -f /usr/share/keyrings/ros-archive-keyring.gpg
+
+# 2. Fetch the key with curl and pipe it through gpg --dearmor
+# This converts the key into the binary format apt expects
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo gpg --dearmor -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+#######################
+
 pip3 install simple_pid
 colcon build --symlink-install
